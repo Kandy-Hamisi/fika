9eb7400603860e6527f116f91246e5430f71ea61
Kandy-Hamisi <hkandy2000@gmail.com>
2025-03-13 22:50:54 +0300

    designed auth pages and clerk setup

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,59 @@
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { icons, images } from "@/constants";
+import InputField from "@/components/InputFied";
+import CustomButton from "@/components/CustomButton";
+import { Link } from "expo-router";
+import OAuth from "@/components/OAuth";
+
+const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  return (
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Welcome Back
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Enter email"
+            icon={icons.email}
+            textContentType="emailAddress"
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            textContentType="password"
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+          <CustomButton title="Sign In" onPress={() => {}} className="mt-6" />
+
+          <OAuth />
+
+          <Link
+            href="/sign-up"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            Don't Have An Account?{" "}
+            <Text className="text-primary-500">Register For Free!</Text>
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+export default SignIn;
